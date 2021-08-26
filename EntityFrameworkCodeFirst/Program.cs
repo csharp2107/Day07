b8b2a0328f51f42e1480861ce79729a1cf26d066
kurs <kurs@DESKTOP-V3HDKPQ>
2021-08-26 10:40:54 +0200

    changes

--- EntityFrameworkCodeFirst/Program.cs
+++ EntityFrameworkCodeFirst/Program.cs
@@ -18,8 +18,14 @@ namespace EntityFrameworkCodeFirst
                 {
                     Console.WriteLine($"{grade.GradeID}, {grade.SchoolSubject}, {grade.GradeValue}");
                 }
+                Console.WriteLine("Grades about CS");
+                var gradesCS = ctx.Grades.Where(x => x.SchoolSubject.Equals("CS"));
+                foreach (var grade in gradesCS)
+                {
+                    Console.WriteLine($"{grade.GradeID}, {grade.SchoolSubject}, {grade.GradeValue}");
+                }
 
-                // creating Student entity
+                // creating simple Student entity
                 var student = new Student()
                 {
                     StudentName = "John Smith",
@@ -27,19 +33,44 @@ namespace EntityFrameworkCodeFirst
                 };
                 ctx.Students.Add(student);
 
-
+                // creating Student entity with some grade
+                // adding 2 grades: Math-3 , CS-5
                 student = new Student()
                 {
                     StudentName = "Ana Smith",
-                    DateOfBirth = new DateTime(2001, 04, 01)
+                    DateOfBirth = new DateTime(2001, 04, 01),                    
                 };
+                // Math-3
+                Grade gradeMath3 = ctx.Grades.Where(x => x.SchoolSubject.Equals("Math") 
+                                        && x.GradeValue == 3).First();
+                // CS-5
+                Grade gradCS5 = ctx.Grades.Where(x => x.SchoolSubject.Equals("CS")
+                                        && x.GradeValue == 5).First();
+                student.Grades = new List<Grade>();
+                if (gradeMath3!=null)
+                    student.Grades.Add(gradeMath3);
+                if (gradCS5!=null) 
+                    student.Grades.Add(gradCS5);
                 ctx.Students.Add(student);
 
+                ctx.SaveChanges(); // store changes to db
+
+                // update DOB for Student with StudentID=1
+                student = ctx.Students.Find(1);
+                student.DateOfBirth = new DateTime(2002, 12, 12);
+                ctx.Entry(student).State = System.Data.Entity.EntityState.Modified;
                 ctx.SaveChanges();
+
+                // delete Student entity with StudentID=2
+                student = ctx.Students.Find(2);
+                //ctx.Entry(student).State = System.Data.Entity.EntityState.Deleted;
+                ctx.Students.Remove(student);
+                ctx.SaveChanges();
+
+                ctx.Database.Connection.Close();
 
                 Console.ReadKey();
 
-                
             }
         }
     }
